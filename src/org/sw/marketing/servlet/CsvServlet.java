99861e11330e8c62c5b6653dc32f68f215dfd3ce
minja-gaso <minja.gaso@outlook.com>
2016-01-19 09:04:22 -0600

    Change generated CSV file code

--- src/org/sw/marketing/servlet/CsvServlet.java
+++ src/org/sw/marketing/servlet/CsvServlet.java
@@ -2,11 +2,14 @@ package org.sw.marketing.servlet;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,6 +30,8 @@ import org.sw.marketing.data.form.Data.Form.Question.PossibleAnswer;
 import org.sw.marketing.data.form.Data.Submission.Answer;
 import org.sw.marketing.transformation.TransformerHelper;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
 import com.opencsv.CSVWriter;
 import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
@@ -45,169 +50,116 @@ public class CsvServlet extends HttpServlet {
 		AnswerDAO answerDAO = DAOFactory.getPossibleAnswerDAO();
 		SubmissionDAO submissionDAO = DAOFactory.getSubmissionDAO();
 		SubmissionAnswerDAO submissionAnswerDAO = DAOFactory.getSubmissionAnswerDAO();
-		
-		Data data = new Data();
-		long formID = Long.parseLong("1161948810985866242");
-		Form form = formDAO.getForm(formID);
-		
-		java.util.List<Submission> submissions = submissionDAO.getSubmissions(formID);	
-		java.util.List<String> headers = new java.util.ArrayList<String>();
-		java.util.List<Long> answeredIds = new java.util.ArrayList<>();
-		java.util.List<Question> questions = questionDAO.getQuestions(formID);
 
 		/*
-		 * add questions to xml
+		 * process submitted form fields
 		 */
-		if(questions != null)
+		ListMultimap<String, String> parameterMap = getFormFields(request);
+		long formID = (long) 0;
+		if(request.getParameter("id") != null)
 		{
-			for(Question question : questions)
+			formID = Long.parseLong(request.getParameter("id"));
+		}
+		else
+		{
+			response.getWriter().println("you must pass a form/survey id");
+			return;
+		}
+		
+		Data data = new Data();
+		Form form = formDAO.getForm(formID);
+		java.util.List<Question> questions = questionDAO.getQuestions(formID);
+		java.util.List<Submission> submissions = submissionDAO.getSubmissions(formID);	
+		java.util.Map<Long, Long> ids = new java.util.LinkedHashMap<>();
+
+		java.util.List<String> headers = new java.util.ArrayList<String>();
+		
+		for(Question question : questions)
+		{
+			String label = "[" + question.getNumber() + "] " + question.getLabel();
+			
+			/*
+			 * if multiple choice
+			 */
+			java.util.List<PossibleAnswer> availableAnswers = answerDAO.getPossibleAnswers(question.getId());
+			if(availableAnswers != null)
 			{
-				long questionID = question.getId();
-				java.util.List<PossibleAnswer> answerList = answerDAO.getPossibleAnswers(questionID);
-				if(answerList != null)
+				java.util.Iterator<PossibleAnswer> iter = availableAnswers.iterator();
+				while(iter.hasNext())
 				{
-					java.util.Iterator<PossibleAnswer> answerListIter = answerList.iterator();
-					while(answerListIter.hasNext())
-					{
-						headers.add(question.getNumber() + ". " + answerListIter.next().getLabel());
-					}
+					PossibleAnswer possibleAnswer = iter.next();
+					label = "[" + question.getNumber() + "] " + possibleAnswer.getLabel();
+					headers.add(question.getNumber() + ". " + possibleAnswer.getLabel());
+					ids.put(possibleAnswer.getId(), possibleAnswer.getId());	
+				}
+			}
+			else
+			{
+				headers.add(question.getNumber() + ". " + question.getLabel());
+				ids.put(question.getId(), question.getId());
+			}		
+		}
+
+		StringWriter strWriter = new StringWriter();
+//		CSVWriter writer = new CSVWriter(new FileWriter("E:\\sample2.csv"), ',');	
+		CSVWriter writer = new CSVWriter(strWriter, ',');
+		writer.writeNext(headers.toArray(new String[headers.size()]));
+		
+		for(Submission submission : submissions)
+		{			
+			java.util.List<String> line = new java.util.ArrayList<String>();
+			for(Entry<Long, Long> entry : ids.entrySet())
+			{
+				long id = entry.getKey();
+				Answer answer = submissionAnswerDAO.getSubmissionAnswer(submission.getId(), id);
+				if(answer != null)
+				{
+					line.add(answer.getAnswerValue());
 				}
 				else
 				{
-					headers.add(question.getNumber() + ". " + question.getLabel());
-				}
-				form.getQuestion().add(question);
-			}
-		}
-
-
-		/*
-		 * add submissions to xml
-		 */
-		if(submissions != null)
-		{
-			java.util.List<String> row = new java.util.ArrayList<String>();
-			for(Submission submission : submissions)
-			{
-				java.util.List<Answer> submissionAnswers = submissionAnswerDAO.getSubmissionAnswers(submission.getId());
-				if(submissionAnswers != null)
-				{
-					for(Answer answer : submissionAnswers)
+					answer = submissionAnswerDAO.getSubmissionAnswerByValue(submission.getId(), id);
+					if(answer != null)
 					{
-						if(answer.isMultipleChoice())
-						{
-							long answerID = Long.parseLong(answer.getAnswerValue());
-							String answerLabel = answerDAO.getPossibleAnswerLabel(answerID);
-							answer.setAnswerLabel(answerLabel);
-							System.err.println("Answer:" + answerLabel + ":::" + answer.getAnswerValue());
-							answeredIds.add(Long.parseLong(answer.getAnswerValue()));
-						}
-					}
-					submission.getAnswer().addAll(submissionAnswers);
-				}
-				data.getSubmission().add(submission);
-			}
-		}
-		data.getForm().add(form);
-
-		String xmlStr = TransformerHelper.getXmlStr("org.sw.marketing.data.form", data);	
-//		System.out.println(xmlStr);
-		
-		try
-		{
-			CSVWriter writer = new CSVWriter(new FileWriter("E:\\sample2.csv"), ',');
-			writer.writeNext(headers.toArray(new String[headers.size()]));
-
-			java.util.List<String> columnCells = new java.util.ArrayList<String>();
-			
-			
-			for(Submission submission : submissions)
-			{
-				java.util.List<String> cellsList = new java.util.ArrayList<String>();
-				java.util.List<Answer> submissionAnswers = submissionAnswerDAO.getSubmissionAnswers(submission.getId());
-				if(submissionAnswers != null)
-				{
-					java.util.Map<Long, Long> questionIdMap = new java.util.LinkedHashMap<Long, Long>();
-					for(Answer answer : submissionAnswers)
-					{
-						questionIdMap.put(answer.getQuestionId(), answer.getQuestionId());
-					}
-					
-					for(Answer answer : submissionAnswers)
-					{
-						if(answer.isMultipleChoice())
-						{
-							long answerID = Long.parseLong(answer.getAnswerValue());
-							
-							if(questionIdMap.get(answer.getQuestionId()) != (long) 0)
-							{
-								String answerLabel = answerDAO.getPossibleAnswerLabel(answerID);
-								List<PossibleAnswer> possibleAnswers = answerDAO.getPossibleAnswers(answer.getQuestionId());
-								java.util.Iterator<PossibleAnswer> possibleAnswersIter = possibleAnswers.iterator();
-								while(possibleAnswersIter.hasNext())
-								{
-									PossibleAnswer possibleAnswer = possibleAnswersIter.next();
-//									if(answerID == possibleAnswer.getId())
-									if(answeredIds.contains(answerID))
-									{
-										cellsList.add("1");
-									}
-									else
-									{
-										cellsList.add("0");
-									}
-									
-									System.out.println("Possible Answer: " + possibleAnswer.getLabel());
-								}
-							}
-							
-							questionIdMap.put(answer.getQuestionId(), (long) 0);
-						}
-						else
-						{
-							cellsList.add(answer.getAnswerValue());
-						}
-					}
-					submission.getAnswer().addAll(submissionAnswers);
-					writer.writeNext(cellsList.toArray(new String[cellsList.size()]));
-				}
-				data.getSubmission().add(submission);
-			}
-			
-			writer.writeNext(columnCells.toArray(new String[columnCells.size()]));
-			
-			writer.close();
-		}
-		catch (IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-//		for()
-
-		for(Submission submission : submissions)
-		{
-			java.util.List<Answer> submissionAnswerList = submissionAnswerDAO.getSubmissionAnswers(submission.getId());
-			if(submissionAnswerList != null)
-			{
-				for(Answer answer : submissionAnswerList)
-				{
-					if(answer.isMultipleChoice())
-					{
-						long answerID = Long.parseLong(answer.getAnswerValue());
-						String answerLabel = answerDAO.getPossibleAnswerLabel(answerID);
-						answer.setAnswerLabel(answerLabel);
-//						System.out.println(answerLabel);
+						line.add("T");
 					}
 					else
 					{
-//						System.out.println(answer.getAnswerValue());
+						line.add("");
 					}
 				}
-				submission.getAnswer().addAll(submissionAnswerList);
+				
 			}
-			data.getSubmission().add(submission);
+			
+			writer.writeNext(line.toArray(new String[line.size()]));
 		}
+		
+//		writer.writeNext(columnCells.toArray(new String[columnCells.size()]));	
+		
+		byte[] csv = strWriter.toString().getBytes();
+		response.setContentType("text/csv");
+		response.setHeader("Content-Disposition", "attachment; filename=data.csv");
+		response.setContentLength(csv.length);
+		ServletOutputStream out = response.getOutputStream();
+		out.write(csv);
+		
+		writer.close();
+	}
+	
+	protected ListMultimap<String, String> getFormFields(HttpServletRequest request)
+	{
+		ListMultimap<String, String> parameterMap = ArrayListMultimap.create();
+		java.util.Enumeration<String> parameterNames = request.getParameterNames();
+		while (parameterNames.hasMoreElements())
+		{
+			String parameterName = (String) parameterNames.nextElement();
+			String[] parameterValue = request.getParameterValues(parameterName);
+			for(int index = 0; index < parameterValue.length; index++)
+			{
+				parameterMap.put(parameterName, parameterValue[index]);
+			}
+		}
+		
+		return parameterMap;
 	}
 }
