cc2d195a3e9f6632fa48eceeb8dd52c556f3ef2e
minja-gaso <minja.gaso@outlook.com>
2016-01-24 13:02:11 -0600

    Add score tracker for self-assessments

--- src/org/sw/marketing/servlet/FormCompletedServlet.java
+++ src/org/sw/marketing/servlet/FormCompletedServlet.java
@@ -10,8 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.sw.marketing.dao.DAOFactory;
 import org.sw.marketing.dao.form.FormDAO;
+import org.sw.marketing.dao.question.QuestionDAO;
+import org.sw.marketing.dao.score.ScoreDAO;
+import org.sw.marketing.dao.submission.SubmissionAnswerDAO;
+import org.sw.marketing.dao.submission.SubmissionDAO;
 import org.sw.marketing.data.form.Data;
 import org.sw.marketing.data.form.Data.Form;
+import org.sw.marketing.data.form.Data.Score;
+import org.sw.marketing.data.form.Data.Submission;
 import org.sw.marketing.transformation.TransformerHelper;
 import org.sw.marketing.util.ReadFile;
 
@@ -23,20 +29,44 @@ public class FormCompletedServlet extends HttpServlet
 
 	protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
+		String SESSION_ID = request.getSession().getId();
+		
+		FormDAO formDAO = DAOFactory.getFormDAO();
+		QuestionDAO questionDAO = DAOFactory.getQuestionDAO();
+		SubmissionDAO submissionDAO = DAOFactory.getSubmissionDAO();
+		SubmissionAnswerDAO submissionAnswerDAO = DAOFactory.getSubmissionAnswerDAO();
+		
 		String paramFormID = request.getPathInfo().substring(1);
 		long formID = Long.parseLong(paramFormID);
-		FormDAO formDAO = DAOFactory.getFormDAO();
 		Data data = new Data();
 		Form form = formDAO.getForm(formID);
-		if(form != null)
+		
+		/*
+		 * initialize submission
+		 */
+		Submission submission = null;
+		if(request.getSession().getAttribute("SUBMISSION") != null)
 		{
-			data.getForm().add(form);
+			submission = (Submission) request.getSession().getAttribute("SUBMISSION");
+			data.getSubmission().add(submission);
 		}
 		
 		if(request.getAttribute("SELF_ASSESSMENT_SCORE") != null)
 		{
 			int score = (int) request.getAttribute("SELF_ASSESSMENT_SCORE");
 			form.setScore(score);
+		}
+
+		if(form != null)
+		{
+			data.getForm().add(form);
+		}
+		
+		ScoreDAO scoreDAO = DAOFactory.getScoreDAO();
+		java.util.List<Score> scores = scoreDAO.getScores(formID);
+		if(scores != null)
+		{
+			data.getScore().addAll(scores);
 		}
 		
 		/*
