44e623fb9d913ff2dcef6403eef4470d940fc83a
minja-gaso <MIG3075@SWT1611-21268.swntdomain.sw.org>
2016-01-27 16:02:07 -0600

    Formatted servlet

--- src/org/sw/marketing/servlet/FormServlet.java
+++ src/org/sw/marketing/servlet/FormServlet.java
@@ -70,27 +70,27 @@ public class FormServlet extends HttpServlet
 		 * process submitted form fields
 		 */
 		ListMultimap<String, String> parameterMap = getFormFields(request);
-		
+
 		/*
 		 * determine action
 		 */
 		String paramAction = null;
 		boolean paramPostForm = false;
-		
-		if(parameterMap != null)
+
+		if (parameterMap != null)
 		{
-			if(parameterMap.get("ACTION") != null && parameterMap.get("ACTION").size() > 0)
+			if (parameterMap.get("ACTION") != null && parameterMap.get("ACTION").size() > 0)
 			{
 				paramAction = parameterMap.get("ACTION").get(0);
 			}
-			if(parameterMap.get("POST_FORM") != null && parameterMap.get("POST_FORM").size() > 0)
+			if (parameterMap.get("POST_FORM") != null && parameterMap.get("POST_FORM").size() > 0)
 			{
 				paramPostForm = Boolean.parseBoolean(parameterMap.get("POST_FORM").get(0));
 			}
 		}
-		
+
 		boolean formSubmitted = false;
-		
+
 		/*
 		 * get form ID
 		 */
@@ -101,23 +101,23 @@ public class FormServlet extends HttpServlet
 		{
 			formID = Long.parseLong(request.getPathInfo().substring(1));
 		}
-		catch(NumberFormatException e)
+		catch (NumberFormatException e)
 		{
 			prettyUrl = true;
 			formPrettyUrl = request.getPathInfo().substring(1);
-		}		
+		}
 
 		Form form = null;
-		if(prettyUrl)
+		if (prettyUrl)
 		{
-			form = formDAO.getFormByPrettyUrl(formPrettyUrl);	
+			form = formDAO.getFormByPrettyUrl(formPrettyUrl);
 		}
 		else
 		{
-			form = formDAO.getForm(formID);			
-		}	
-		
-		if(form != null)
+			form = formDAO.getForm(formID);
+		}
+
+		if (form != null)
 		{
 			formID = form.getId();
 		}
@@ -126,12 +126,12 @@ public class FormServlet extends HttpServlet
 			response.getWriter().println("The form you are looking for could not be found.");
 			return;
 		}
-		
+
 		/*
 		 * initialize page variable
 		 */
 		int lastPage = questionDAO.getLatestPage(formID);
-		if(lastPage == 0)
+		if (lastPage == 0)
 		{
 			lastPage = 1;
 		}
@@ -139,46 +139,66 @@ public class FormServlet extends HttpServlet
 		int previousPage = 1;
 		try
 		{
-			if(parameterMap.get("CURRENT_PAGE") != null && parameterMap.get("CURRENT_PAGE").size() > 0)
+			if (parameterMap.get("CURRENT_PAGE") != null && parameterMap.get("CURRENT_PAGE").size() > 0)
 			{
 				currentPage = Integer.parseInt(parameterMap.get("CURRENT_PAGE").get(0));
 			}
-			if(parameterMap.get("PREVIOUS_PAGE") != null && parameterMap.get("PREVIOUS_PAGE").size() > 0)
+			if (parameterMap.get("PREVIOUS_PAGE") != null && parameterMap.get("PREVIOUS_PAGE").size() > 0)
 			{
 				previousPage = Integer.parseInt(parameterMap.get("PREVIOUS_PAGE").get(0));
 			}
 		}
-		catch(NumberFormatException e)
+		catch (NumberFormatException e)
 		{
 			e.printStackTrace();
 		}
 		
+		java.util.List<Question> questionList = new java.util.ArrayList<Question>();
+		java.util.List<Message> messageList = null;
+
+		/*
+		 * determine current page thru action param
+		 */
+		boolean goPreviousPage = false;
+		boolean goNextPage = false;
+		if (messageList == null)
+		{
+			if (paramAction != null && paramAction.equals("NEXT_PAGE"))
+			{
+				currentPage = previousPage + 1;
+				goNextPage = true;
+			}
+			else if (paramAction != null && paramAction.equals("PREVIOUS_PAGE"))
+			{
+				currentPage = previousPage - 1;
+				goPreviousPage = true;
+			}
+		}
+
 		/*
 		 * initialize submission
 		 */
 		Submission submission = tempSubmissionDAO.getSubmissionBySessionID(formID, SESSION_ID);
-		if(submission == null)
+		if (submission == null)
 		{
 			tempSubmissionDAO.insert(formID, SESSION_ID);
 			submission = tempSubmissionDAO.getSubmissionBySessionID(formID, SESSION_ID);
 		}
-		
+
 		/*
 		 * determine whether to store submitted answers or send error
 		 */
-		java.util.List<Question> questionList = new java.util.ArrayList<Question>();
-		java.util.List<Message> messageList = null;
-		if(currentPage >= previousPage)
+		if (goNextPage)
 		{
-			for(String key : parameterMap.keySet())
+			for (String key : parameterMap.keySet())
 			{
-				if(key.startsWith("QUESTION_"))
+				if (key.startsWith("QUESTION_"))
 				{
 					Question question = questionDAO.getQuestion(Long.parseLong(key.split("QUESTION_")[1]));
 					questionList.add(question);
-					
+
 					Message message = null;
-					if(question.getType().contains("text"))
+					if (question.getType().contains("text"))
 					{
 						message = processQuestion(question, parameterMap.get(key).get(0));
 					}
@@ -187,25 +207,25 @@ public class FormServlet extends HttpServlet
 						java.util.List<String> answers = parameterMap.get(key);
 						java.util.Iterator<String> answerIter = answers.iterator();
 						boolean atLeastOneChecked = false;
-						while(answerIter.hasNext())
+						while (answerIter.hasNext() )
 						{
 							String answerStr = answerIter.next();
 							message = processQuestion(question, answerStr);
-							if(message == null)
+							if (message == null)
 							{
 								atLeastOneChecked = true;
 							}
 						}
-						
-						if(atLeastOneChecked)
+
+						if (atLeastOneChecked)
 						{
 							message = null;
 						}
 					}
-					
-					if(message != null)
+
+					if (message != null)
 					{
-						if(messageList == null)
+						if (messageList == null)
 						{
 							messageList = new java.util.ArrayList<Message>();
 						}
@@ -214,97 +234,81 @@ public class FormServlet extends HttpServlet
 				}
 			}
 		}
-		
-		/*
-		 * determine current page thru action param
-		 */
-		if(messageList == null)
+
+		submission.setPage(previousPage);
+
+		if (tempSubmissionAnswerDAO.getSubmissionAnswersByPage(submission) != null && goNextPage)
 		{
-			if(paramAction != null && paramAction.equals("NEXT_PAGE"))
-			{
-				currentPage = previousPage + 1;
-			}
-			else if(paramAction != null && paramAction.equals("PREVIOUS_PAGE"))
-			{
-				currentPage = previousPage - 1;
-			}
+			tempSubmissionAnswerDAO.deleteSubmissionAnswersByPage(submission);
 		}
-			
-			submission.setPage(previousPage);
-			
-			
-			if(tempSubmissionAnswerDAO.getSubmissionAnswersByPage(submission) != null)
+
+		/*
+		 * insert questions into temp answers table
+		 */
+		for (String key : parameterMap.keySet())
+		{
+			if (key.startsWith("QUESTION_"))
 			{
-				tempSubmissionAnswerDAO.deleteSubmissionAnswersByPage(submission);
-			}
-			
-			/*
-			 * insert questions into temp answers table
-			 */
-			for(String key : parameterMap.keySet())
-			{
-				if(key.startsWith("QUESTION_"))
+				Question question = questionDAO.getQuestion(Long.parseLong(key.split("QUESTION_")[1]));
+				questionList.add(question);
+
+				java.util.Iterator<String> answerIter = parameterMap.get(key).iterator();
+				while (answerIter.hasNext())
 				{
-					Question question = questionDAO.getQuestion(Long.parseLong(key.split("QUESTION_")[1]));
-					questionList.add(question);
-					
-					java.util.Iterator<String> answerIter = parameterMap.get(key).iterator();
-					while(answerIter.hasNext())
+					String answerStr = answerIter.next();
+					if (answerStr.length() > 0)
 					{
-						String answerStr = answerIter.next();
-						if(answerStr.length() > 0)
+						Answer answer = new Answer();
+						answer.setQuestionId(question.getId());
+						if (question.getType().startsWith("text"))
 						{
-							Answer answer = new Answer();
-							answer.setQuestionId(question.getId());
-							if(question.getType().startsWith("text"))
-							{
-								answer.setMultipleChoice(false);
-							}
-							else
-							{
-								answer.setMultipleChoice(true);
-							}
-							answer.setAnswerValue(answerStr);
-							
-							
-							if(form.getStatus().equals("live"))
-							{
-								tempSubmissionAnswerDAO.insert(submission, answer, question);
-							}
-							else
-							{
-								System.out.println("Not live!");
-							}
+							answer.setMultipleChoice(false);
+						}
+						else
+						{
+							answer.setMultipleChoice(true);
+						}
+						answer.setAnswerValue(answerStr);
+
+						if (form.getStatus().equals("live"))
+						{
+							tempSubmissionAnswerDAO.insert(submission, answer, question);
+						}
+						else
+						{
+							System.out.println("Not live!");
 						}
 					}
 				}
 			}
-			
-			submission.setPage(currentPage);
-			if(tempSubmissionAnswerDAO.getSubmissionAnswersByPage(submission) != null)
-			{
-				submission.getAnswer().addAll(tempSubmissionAnswerDAO.getSubmissionAnswersByPage(submission));
-			}
-		if(messageList == null)
+		}
+
+		submission.setPage(currentPage);
+		if (tempSubmissionAnswerDAO.getSubmissionAnswersByPage(submission) != null)
 		{
-			if(paramAction != null && paramAction.equals("SUBMIT_FORM"))
+			submission.getAnswer().addAll(tempSubmissionAnswerDAO.getSubmissionAnswersByPage(submission));
+		}
+		if (messageList == null)
+		{
+			if (paramAction != null && paramAction.equals("SUBMIT_FORM"))
 			{
 				/*
-				 * don't check if survey live so that you can still see thank you screen!
+				 * don't check if survey live so that you can still see thank
+				 * you screen!
 				 */
 				formSubmitted = true;
-				
-				if(form.getStatus().equals("live"))
+
+				if (form.getStatus().equals("live"))
 				{
 					tempSubmissionDAO.copyTo(SESSION_ID, formID);
 					tempSubmissionAnswerDAO.copyTo(submission);
-					
+
 					/*
 					 * fk constraint - delete answers first!
 					 */
 					tempSubmissionAnswerDAO.deleteFromTemp(submission);
 					tempSubmissionDAO.deleteFromTemp(SESSION_ID, formID);
-				}				
+				}
 			}
 		}
 		else
@@ -316,35 +320,35 @@ public class FormServlet extends HttpServlet
 		 * prepare data
 		 */
 		Data data = new Data();
-		
+
 		/*
 		 * get form data
 		 */
 		java.util.List<Question> currentPageQuestions = questionDAO.getQuestionsByPage(formID, currentPage);
-		
+
 		/*
 		 * if errors exist add to data set
 		 */
-		if(messageList != null)
+		if (messageList != null)
 		{
 			data.getMessage().addAll(messageList);
 		}
-		
-		if(submission != null)
+
+		if (submission != null)
 		{
 			data.getSubmission().add(submission);
 		}
-		
-		if(form.getType().equals("survey"))
+
+		if (form.getType().equals("survey"))
 		{
-			if(currentPageQuestions != null)
+			if (currentPageQuestions != null)
 			{
 				java.util.Iterator<Question> questionIter = currentPageQuestions.iterator();
-				while(questionIter.hasNext())
+				while (questionIter.hasNext())
 				{
 					Question question = questionIter.next();
 					java.util.List<PossibleAnswer> answerList = answerDAO.getPossibleAnswers(question.getId());
-					if(answerList != null)
+					if (answerList != null)
 					{
 						question.getPossibleAnswer().addAll(answerList);
 					}
@@ -352,15 +356,15 @@ public class FormServlet extends HttpServlet
 				form.getQuestion().addAll(currentPageQuestions);
 			}
 		}
-		else if(form.getType().equals("self_assessment"))
+		else if (form.getType().equals("self_assessment"))
 		{
-			if(currentPageQuestions != null)
+			if (currentPageQuestions != null)
 			{
 				form.getQuestion().addAll(currentPageQuestions);
 			}
-			
+
 			java.util.List<Data.PossibleAnswer> possibleAnswers = answerDAO.getPossibleAnswersByForm(formID);
-			if(possibleAnswers != null)
+			if (possibleAnswers != null)
 			{
 				data.getPossibleAnswer().addAll(possibleAnswers);
 			}
@@ -375,11 +379,11 @@ public class FormServlet extends HttpServlet
 		 */
 		TransformerHelper transformerHelper = new TransformerHelper();
 		transformerHelper.setUrlResolverBaseUrl(getServletContext().getInitParameter("assetXslFormsPath"));
-		
-		String xmlStr = transformerHelper.getXmlStr("org.sw.marketing.data.form", data);	
-		
+
+		String xmlStr = transformerHelper.getXmlStr("org.sw.marketing.data.form", data);
+
 		String xslScreen = getServletContext().getInitParameter("assetXslPath");
-		if(form.getType().equals("survey"))
+		if (form.getType().equals("survey"))
 		{
 			xslScreen = xslScreen + "form.xsl";
 		}
@@ -389,18 +393,18 @@ public class FormServlet extends HttpServlet
 		}
 		String xslStr = ReadFile.getSkin(xslScreen);
 		String htmlStr = transformerHelper.getHtmlStr(xmlStr, new ByteArrayInputStream(xslStr.getBytes()));
-		
+
 		String emailScreen = getServletContext().getInitParameter("assetXslPath") + "email_submission.xsl";
 		String emailTemplateStr = ReadFile.getSkin(emailScreen);
 		String emailStr = transformerHelper.getHtmlStr(xmlStr, new ByteArrayInputStream(emailTemplateStr.getBytes()));
-	
+
 		String toolboxSkinPath = getServletContext().getInitParameter("assetPath") + "toolbox_1col.html";
 		String skinHtmlStr = null;
-		
+
 		String skinUrl = form.getSkinUrl();
 		String skinCssSelector = form.getSkinSelector();
-		
-		if(skinUrl.length() > 0 && skinCssSelector.length() > 0)
+
+		if (skinUrl.length() > 0 && skinCssSelector.length() > 0)
 		{
 			skinHtmlStr = SkinReader.getSkinByUrl(form);
 		}
@@ -408,37 +412,43 @@ public class FormServlet extends HttpServlet
 		{
 			skinHtmlStr = ReadFile.getSkin(toolboxSkinPath);
 		}
-		
+
 		skinHtmlStr = skinHtmlStr.replace("{TITLE}", form.getTitle());
 		skinHtmlStr = skinHtmlStr.replace("{CONTENT}", htmlStr);
 
-		if(displayXml)
+		if (displayXml)
 		{
 			System.out.println(xmlStr);
 		}
 		
+		if(request.getParameter("XSL") != null && request.getParameter("XSL").equals("NONE"))
+		{
+			response.setContentType("text/xml");
+			response.getWriter().println(xmlStr);
+			return;
+		}
 
-		if(formSubmitted)
-		{			
-			if(form.getStatus().equals("live"))
+		if (formSubmitted)
+		{
+			if (form.getStatus().equals("live"))
 			{
 				int count = form.getSubmissionCount() + 1;
 				formDAO.updateFormSubmissionCount(formID, count);
 			}
-			
+
 			String redirectId = "" + formID;
-			if(prettyUrl)
+			if (prettyUrl)
 			{
 				redirectId = form.getPrettyUrl();
 			}
-			
+
 			response.sendRedirect(getServletContext().getContextPath() + "/completed/" + redirectId);
 			return;
-		}			
+		}
 
 		response.getWriter().println(skinHtmlStr);
 	}
-	
+
 	protected ListMultimap<String, String> getFormFields(HttpServletRequest request)
 	{
 		ListMultimap<String, String> parameterMap = ArrayListMultimap.create();
@@ -447,19 +457,19 @@ public class FormServlet extends HttpServlet
 		{
 			String parameterName = (String) parameterNames.nextElement();
 			String[] parameterValue = request.getParameterValues(parameterName);
-			for(int index = 0; index < parameterValue.length; index++)
+			for (int index = 0; index < parameterValue.length; index++)
 			{
 				parameterMap.put(parameterName, parameterValue[index]);
 			}
 		}
-		
+
 		return parameterMap;
 	}
-	
+
 	protected Message processQuestion(Question question, String answer)
 	{
 		Message message = null;
-		if(question.isRequired() && answer.length() == 0)
+		if (question.isRequired() && answer.length() == 0)
 		{
 			message = new Message();
 			message.setQuestionId(question.getId());
@@ -468,13 +478,13 @@ public class FormServlet extends HttpServlet
 			message.setSubtype("required");
 			message.setLabel("Question " + question.getNumber() + " is required.");
 		}
-		else if(question.getFilter().equals("email"))
+		else if (question.getFilter().equals("email"))
 		{
 			EmailValidator emailValidator = EmailValidator.getInstance();
 			/*
 			 * if question is required and the answer is not a valid email
 			 */
-			if(!emailValidator.isValid(answer) && question.isRequired())
+			if (!emailValidator.isValid(answer) && question.isRequired())
 			{
 				message = new Message();
 				message.setQuestionId(question.getId());
@@ -486,7 +496,7 @@ public class FormServlet extends HttpServlet
 			/*
 			 * if question is not required but is answered with an invalid email
 			 */
-			else if(!emailValidator.isValid(answer) && !question.isRequired() && answer.length() > 0)
+			else if (!emailValidator.isValid(answer) && !question.isRequired() && answer.length() > 0)
 			{
 				message = new Message();
 				message.setQuestionId(question.getId());
@@ -496,16 +506,16 @@ public class FormServlet extends HttpServlet
 				message.setLabel("If you are going to answer Question " + question.getNumber() + " , please enter a valid email address.");
 			}
 		}
-		else if(question.getFilter().equals("date"))
+		else if (question.getFilter().equals("date"))
 		{
 			Pattern pattern = Pattern.compile("(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])/((19|20)\\d\\d)");
 			Matcher matcher = pattern.matcher(answer);
 			/*
 			 * if question is required and answer doesn't match desired format
 			 */
-			if(question.isRequired())
+			if (question.isRequired())
 			{
-				if(!matcher.matches())
+				if (!matcher.matches())
 				{
 					message = new Message();
 					message.setQuestionId(question.getId());
@@ -515,9 +525,9 @@ public class FormServlet extends HttpServlet
 					message.setLabel("Question " + question.getNumber() + " date is invalid.");
 				}
 			}
-			else if(!question.isRequired() && answer.length() > 0)
+			else if (!question.isRequired() && answer.length() > 0)
 			{
-				if(!matcher.matches())
+				if (!matcher.matches())
 				{
 					message = new Message();
 					message.setQuestionId(question.getId());
@@ -528,7 +538,7 @@ public class FormServlet extends HttpServlet
 				}
 			}
 		}
-		
+
 		return message;
 	}
 
